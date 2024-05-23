f32ff0ba41f9a61045c6fc878da71eceeb1bd447
Janet Gloria <commsult@JanetGloriasMBP.fritz.box>
2024-05-23 23:12:16 +0200

    working register

--- lib/pages/register/register.dart
+++ lib/pages/register/register.dart
@@ -1,242 +1,5 @@
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-// import 'package:fixaxi/constants/colors.dart';
-
-// class SignUpPage extends StatelessWidget {
-//   SignUpPage({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final screenWidth = MediaQuery.of(context).size.width;
-//     final screenHeight = MediaQuery.of(context).size.height;
-//     return Scaffold(
-//       body: Container(
-//         decoration: const BoxDecoration(
-//           // image: DecorationImage(image: AssetImage('assets/LoginPageBG.jpg'), fit: BoxFit.cover)
-//           gradient: LinearGradient(
-//             begin: Alignment.topRight,
-//             end: Alignment.bottomLeft,
-//             colors: [
-//               AppColors.blue4,
-//               Colors.white,
-//             ],
-//           ),
-//         ),
-//         child: Padding(
-//           padding: const EdgeInsets.only(
-//             top: 150,
-//           ),
-//           child: Center(
-//             child: Container(
-//               width: screenWidth,
-//               decoration: const BoxDecoration(
-//                 borderRadius: BorderRadius.only(
-//                     topLeft: Radius.circular(20),
-//                     topRight: Radius.circular(20)),
-//                 color: Colors.white,
-//               ),
-//               child: Column(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                   //Welcome Text
-//                   const Text(
-//                     "Welcome to Fixaxi",
-//                     style: TextStyle(
-//                         fontWeight: FontWeight.bold,
-//                         fontSize: 24,
-//                         fontFamily: "Cupertino"),
-//                   ),
-//                   //SizedBox
-//                   const SizedBox(height: 10),
-//                   //Sign in credentials Text
-//                   const Text(
-//                     "Please sign up with your credentials below",
-//                   ),
-//                   //SizedBox
-//                   const SizedBox(
-//                     height: 20,
-//                   ),
-//                   //First Name Input Field
-//                   Padding(
-//                     padding: const EdgeInsets.symmetric(horizontal: 25),
-//                     child: Container(
-//                       decoration: BoxDecoration(
-//                         color: Colors.grey[100],
-//                         border: Border.all(color: Colors.white),
-//                         borderRadius: BorderRadius.circular(12),
-//                       ),
-//                       child: const Padding(
-//                         padding: EdgeInsets.only(left: 20.0),
-//                         child: TextField(
-//                           obscureText: true,
-//                           decoration: InputDecoration(
-//                             border: InputBorder.none,
-//                             hintText: "First Name",
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                   //SizedBox
-//                   const SizedBox(
-//                     height: 10,
-//                   ),
-//                   //Last Name Input Field
-//                   Padding(
-//                     padding: const EdgeInsets.symmetric(horizontal: 25),
-//                     child: Container(
-//                       decoration: BoxDecoration(
-//                         color: Colors.grey[100],
-//                         border: Border.all(color: Colors.white),
-//                         borderRadius: BorderRadius.circular(12),
-//                       ),
-//                       child: const Padding(
-//                         padding: EdgeInsets.only(left: 20.0),
-//                         child: TextField(
-//                           obscureText: true,
-//                           decoration: InputDecoration(
-//                             border: InputBorder.none,
-//                             hintText: "Last Name",
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                   //SizedBox
-//                   const SizedBox(
-//                     height: 10,
-//                   ),
-//                   //Date of Birth Input Field
-//                   Padding(
-//                     padding: const EdgeInsets.symmetric(horizontal: 25),
-//                     child: Container(
-//                       decoration: BoxDecoration(
-//                         color: Colors.grey[100],
-//                         border: Border.all(color: Colors.white),
-//                         borderRadius: BorderRadius.circular(12),
-//                       ),
-//                       child: const Padding(
-//                         padding: EdgeInsets.only(left: 20.0),
-//                         child: TextField(
-//                           obscureText: true,
-//                           decoration: InputDecoration(
-//                             border: InputBorder.none,
-//                             hintText: "Date of Birth",
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                   //SizedBox
-//                   const SizedBox(
-//                     height: 10,
-//                   ),
-//                   //Email Input Field
-//                   Padding(
-//                     padding: const EdgeInsets.symmetric(horizontal: 25),
-//                     child: Container(
-//                       decoration: BoxDecoration(
-//                         color: Colors.grey[100],
-//                         border: Border.all(color: Colors.white),
-//                         borderRadius: BorderRadius.circular(12),
-//                       ),
-//                       child: const Padding(
-//                         padding: EdgeInsets.only(left: 20.0),
-//                         child: TextField(
-//                           obscureText: true,
-//                           decoration: InputDecoration(
-//                             border: InputBorder.none,
-//                             hintText: "Email",
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                   //SizedBox
-//                   const SizedBox(
-//                     height: 10,
-//                   ),
-//                   //Password Input Field
-//                   Padding(
-//                     padding: const EdgeInsets.symmetric(horizontal: 25),
-//                     child: Container(
-//                       decoration: BoxDecoration(
-//                         color: Colors.grey[100],
-//                         border: Border.all(color: Colors.white),
-//                         borderRadius: BorderRadius.circular(12),
-//                       ),
-//                       child: const Padding(
-//                         padding: EdgeInsets.only(left: 20.0),
-//                         child: TextField(
-//                           decoration: InputDecoration(
-//                             border: InputBorder.none,
-//                             hintText: "Password",
-//                           ),
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                   //SizedBox
-//                   const SizedBox(height: 10.0),
-//                   //Sign in Button
-//                   Padding(
-//                     padding: const EdgeInsets.symmetric(horizontal: 25),
-//                     child: Container(
-//                       padding: const EdgeInsets.all(15.0),
-//                       decoration: BoxDecoration(
-//                         gradient: const LinearGradient(
-//                           begin: Alignment.topRight,
-//                           end: Alignment.bottomLeft,
-//                           colors: [
-//                             AppColors.blue6,
-//                             AppColors.blue2,
-//                           ],
-//                         ),
-//                         borderRadius: BorderRadius.circular(12),
-//                       ),
-//                       child: const Center(
-//                         child: Text(
-//                           "Sign Up",
-//                           style: TextStyle(
-//                               color: Colors.white,
-//                               fontSize: 16,
-//                               fontWeight: FontWeight.bold),
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                   //SizedBox
-//                   const SizedBox(height: 10),
-//                   Row(
-//                     mainAxisAlignment: MainAxisAlignment.center,
-//                     children: [
-//                       const Text(
-//                         "Already a member?",
-//                         style: TextStyle(fontWeight: FontWeight.bold),
-//                       ),
-//                       const SizedBox(
-//                         width: 10,
-//                       ),
-//                       Text(
-//                         "Sign In Now",
-//                         style: TextStyle(
-//                             color: Colors.blue[300],
-//                             fontWeight: FontWeight.bold),
-//                       ),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
+import 'package:fixaxi/pages/login/login.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:fixaxi/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -345,7 +108,7 @@ class _SignUpPageState extends State<SignUpPage> {
                     ),
                   ),
                   const SizedBox(height: 10),
-                  Row(
+                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       const Text(
@@ -353,12 +116,20 @@ class _SignUpPageState extends State<SignUpPage> {
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                       const SizedBox(width: 10),
-                      Text(
-                        "Sign In Now",
-                        style: TextStyle(
-                            color: Colors.blue[300],
-                            fontWeight: FontWeight.bold),
-                      ),
+                      GestureDetector(
+  onTap: () {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SignInPage()),
+    );
+  },
+  child: Text(
+    "Sign In Now",
+    style: TextStyle(
+        color: Colors.blue[300],
+        fontWeight: FontWeight.bold),
+  ),
+)
                     ],
                   ),
                 ],
@@ -416,6 +187,7 @@ class _SignUpPageState extends State<SignUpPage> {
               setState(() {});
             },
             child: Text(
+              // ignore: unnecessary_null_comparison
               dob == null ? hintText : dob.toIso8601String().substring(0, 10),
             ),
           ),
@@ -427,7 +199,6 @@ class _SignUpPageState extends State<SignUpPage> {
   Future<void> signUp() async {
     final String firstName = firstNameController.text.trim();
     final String lastName = lastNameController.text.trim();
-    // final String dob = dob.toIso8601String().substring(0, 10);
     final String dobString = dob.toIso8601String().substring(0, 10);
     final String email = emailController.text.trim();
     final String password = passwordController.text.trim();
